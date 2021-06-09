c25feb6f381b7538e58d38e22c230ea36b946fc0
kebiro <bekiroe@hotmail.de>
2021-06-09 03:05:10 +0200

    Run a compute from start to finish using pasture_core::gpu::device

--- pasture-core/examples/test_device.rs
+++ pasture-core/examples/test_device.rs
@@ -1,19 +1,138 @@
 use pasture_core::gpu;
-use pasture_core::gpu::DevicePower;
+use pasture_core::nalgebra::Vector3;
+use pasture_core::containers::{PerAttributeVecPointStorage, PerAttributePointBufferMutExt, PointBuffer};
+use pasture_derive::PointType;
+use pasture_core::layout::{attributes, PointAttributeDefinition, PointAttributeDataType};
+use pasture_core::layout::PointType;
+use bytemuck::__core::convert::TryInto;
+
+// Custom PointLayout
+#[derive(PointType, Debug)]
+#[repr(C)]
+struct MyPointType {
+    #[pasture(BUILTIN_POSITION_3D)]
+    pub position: Vector3<f64>,
+    #[pasture(BUILTIN_COLOR_RGB)]
+    pub icolor: Vector3<u16>,
+    #[pasture(attribute = "MyColorF32")]
+    pub fcolor: Vector3<f32>,
+}
 
 fn main() {
     futures::executor::block_on(run());
 }
 
 async fn run() {
+    // == Init point buffer ======================================================================
+
+    let points = vec![
+        MyPointType {
+            position: Vector3::new(1.0, 0.0, 0.0),
+            icolor: Vector3::new(255, 0, 0),
+            fcolor: Vector3::new(1.0, 0.0, 0.0),
+        },
+        MyPointType {
+            position: Vector3::new(0.0, 1.0, 0.0),
+            icolor: Vector3::new(0, 255, 0),
+            fcolor: Vector3::new(0.0, 1.0, 0.0),
+        },
+        MyPointType {
+            position: Vector3::new(0.0, 0.0, 1.0),
+            icolor: Vector3::new(0, 0, 255),
+            fcolor: Vector3::new(0.0, 0.0, 1.0),
+        },
+    ];
+
+    let layout = MyPointType::layout();
+    let mut point_buffer = PerAttributeVecPointStorage::new(layout);
+    point_buffer.push_points(points.as_slice());
+
+    let custom_color_attrib =
+        PointAttributeDefinition::custom("MyColorF32", PointAttributeDataType::Vec3f32);
+
+    // == GPU ====================================================================================
+
+    // Create a device
+
     let device = gpu::Device::default().await;
     device.print_device_info();
 
-    let device = gpu::Device::new(
+    let mut device = gpu::Device::new(
         gpu::DeviceOptions {
             device_power: gpu::DevicePower::High,
-            device_backend: gpu::DeviceBackend::Dx12,
+            device_backend: gpu::DeviceBackend::Vulkan,
         }
     ).await;
     device.print_device_info();
+
+    // Put data onto buffers
+
+    // TODO: this may be useful when trying to create buffer from just the layout
+    // println!("{:?}", point_buffer.point_layout());
+
+    let positions = point_buffer.get_attribute_range_mut::<Vector3<f64>>(
+        0..point_buffer.len(),
+        &attributes::POSITION_3D)
+        .iter()
+        .flat_map(|v| vec![v.x, v.y, v.z, 1.0].into_iter())
+        .collect::<Vec<f64>>();
+    println!("Positions: {:?}", positions);
+
+    // Shaders do not support 16-bit types... for now convert to u32 to avoid alignment issues
+    let icolors = point_buffer.get_attribute_range_mut::<Vector3<u16>>(
+        0..point_buffer.len(),
+        &attributes::COLOR_RGB)
+        .iter()
+        .flat_map(|c| vec![c.x as u32, c.y as u32, c.z as u32, 255].into_iter())
+        .collect::<Vec<u32>>();
+    println!("Colors (u16): {:?}", icolors);
+
+    let fcolors = point_buffer.get_attribute_range_mut::<Vector3<f32>>(
+        0..point_buffer.len(),
+        &custom_color_attrib)
+        .iter()
+        .flat_map(|c| vec![c.x, c.y, c.z, 1.0].into_iter())
+        .collect::<Vec<f32>>();
+    println!("Colors (f32): {:?}", fcolors);
+
+    let device_buffers = vec![
+        gpu::DeviceBuffer {
+            content: bytemuck::cast_slice(&positions),
+            binding: 0
+        },
+        gpu::DeviceBuffer {
+            content: bytemuck::cast_slice(&icolors),
+            binding: 1
+        },
+        gpu::DeviceBuffer {
+            content: bytemuck::cast_slice(&fcolors),
+            binding: 2
+        },
+    ];
+
+    device.upload(device_buffers);
+    device.set_compute_shader(include_str!("device.comp"));
+    device.compute(1, 1, 1);
+    println!("\n===== COMPUTE =====\n");
+
+    let results_as_bytes = device.download().await;
+
+    let pos_result_vec: Vec<f64> = results_as_bytes[0]
+        .chunks_exact(8)
+        .map(|b| f64::from_ne_bytes(b.try_into().unwrap()))
+        .collect();
+    println!("Positions: {:?}", pos_result_vec);
+
+    // TODO: need to convert back to u16
+    let icol_result_vec: Vec<u32> = results_as_bytes[1]
+        .chunks_exact(4)
+        .map(|b| u32::from_ne_bytes(b.try_into().unwrap()))
+        .collect();
+    println!("Colors (u16): {:?}", icol_result_vec);
+
+    let fcol_result_vec: Vec<f32> = results_as_bytes[2]
+        .chunks_exact(4)
+        .map(|b| f32::from_ne_bytes(b.try_into().unwrap()))
+        .collect();
+    println!("Colors (f32): {:?}", fcol_result_vec);
 }
--- pasture-core/src/gpu/device.rs
+++ pasture-core/src/gpu/device.rs
@@ -1,9 +1,20 @@
+use wgpu::util::{DeviceExt, BufferInitDescriptor};
+use wgpu::BufferDescriptor;
 
 pub struct Device {
     // Private fields
     adapter: wgpu::Adapter,
     device: wgpu::Device,
     queue: wgpu::Queue,
+
+    upload_buffers: Vec<wgpu::Buffer>,
+    download_buffers: Vec<wgpu::Buffer>,
+    buffer_sizes: Vec<wgpu::BufferAddress>,
+    buffer_bindings: Vec<u32>,
+
+    cs_module: Option<wgpu::ShaderModule>,
+    bind_group: Option<wgpu::BindGroup>,
+    compute_pipeline: Option<wgpu::ComputePipeline>,
 }
 
 impl Device {
@@ -63,10 +74,28 @@ impl Device {
             None,
         ).await.unwrap();
 
+        // == Initially empty buffers =============================================================
+
+        let upload_buffers: Vec<wgpu::Buffer> = Vec::new();
+        let download_buffers: Vec<wgpu::Buffer> = Vec::new();
+        let buffer_sizes: Vec<wgpu::BufferAddress> = Vec::new();
+        let buffer_bindings: Vec<u32> = Vec::new();
+
+        let cs_module = Option::None;
+        let bind_group = Option::None;
+        let compute_pipeline = Option::None;
+
         Device {
             adapter,
             device,
-            queue
+            queue,
+            upload_buffers,
+            download_buffers,
+            buffer_sizes,
+            buffer_bindings,
+            cs_module,
+            bind_group,
+            compute_pipeline,
         }
     }
 
@@ -81,10 +110,201 @@ impl Device {
         println!("PCI id: {}", info.device);
         println!("Vendor PCI id: {}\n", info.vendor);
     }
+
+    pub fn upload(&mut self, device_buffers: Vec<DeviceBuffer>) {
+        // - Take a list of DeviceBuffer as input
+        // - Internally create appropriate wgpu::Buffer objects
+        // - Put those in a list and store in self.upload_buffers and self.download_buffers
+
+        for buffer in device_buffers {
+            // TODO: for now assume corresponding buffers have same index
+
+            // println!("{:?}", buffer.content.len());
+            let size_in_bytes = buffer.content.len() as wgpu::BufferAddress;
+            self.buffer_sizes.push(size_in_bytes);
+
+            self.upload_buffers.push(self.device.create_buffer_init(
+               &BufferInitDescriptor {
+                   label: None,
+                   contents: buffer.content,
+                   usage: wgpu::BufferUsage::STORAGE | wgpu::BufferUsage::COPY_SRC
+               }
+            ));
+
+            self.download_buffers.push(self.device.create_buffer(
+                &BufferDescriptor {
+                    label: None,
+                    size: size_in_bytes,
+                    usage: wgpu::BufferUsage::MAP_READ | wgpu::BufferUsage::COPY_DST,
+                    mapped_at_creation: false
+                }
+            ));
+
+            self.buffer_bindings.push(buffer.binding);
+        }
+    }
+
+    pub async fn download(&self) -> Vec<Vec<u8>> {
+        //TODO:
+        // - Get content from self.download_buffers
+        // - Write and return updated layout
+        let mut output_bytes: Vec<Vec<u8>> = Vec::new();
+
+        for i in 0..self.download_buffers.len() {
+            let download = self.download_buffers.get(i).unwrap();
+
+            let result_buffer_slice = download.slice(..);
+            let result_buffer_future = result_buffer_slice.map_async(wgpu::MapMode::Read);
+            self.device.poll(wgpu::Maintain::Wait); // Should be called in event loop or other thread ...
+
+            // TODO: how to know the data type of the current buffer?
+            if let Ok(()) = result_buffer_future.await {
+                let result_as_bytes = result_buffer_slice.get_mapped_range();
+                &output_bytes.push(result_as_bytes.to_vec());
+
+                // Drop all mapped views before unmapping buffer
+                drop(result_as_bytes);
+                download.unmap();
+            }
+        };
+
+        output_bytes
+    }
+
+    pub fn set_compute_shader(&mut self, compute_shader_file_path: &str) {
+        self.cs_module = self.compile_and_create_compute_module(compute_shader_file_path);
+
+        let (bind_group, pipeline)
+            = self.create_compute_pipeline(self.cs_module.as_ref().unwrap());
+
+        self.bind_group = Some(bind_group);
+        self.compute_pipeline = Some(pipeline);
+    }
+
+    fn compile_and_create_compute_module(&self, cs_src: &str) -> Option<wgpu::ShaderModule> {
+        // WebGPU wants its shaders pre-compiled in binary SPIR-V format.
+        // So we'll take the source code of our compute shader and compile it
+        // with the help of the shaderc crate.
+
+        // let cs_src = include_str!(compute_shader_file_path.to_string());
+        // let cs_src = fs::read_to_string(&compute_shader_file_path).unwrap();
+        let mut compiler = shaderc::Compiler::new().unwrap();
+        let cs_spirv = compiler
+            .compile_into_spirv(
+                cs_src,
+                shaderc::ShaderKind::Compute,
+                "Compute shader",
+                "main",
+                None,
+            )
+            .unwrap();
+        let cs_data = wgpu::util::make_spirv(cs_spirv.as_binary_u8());
+
+        // Now with the binary data we can create and return our ShaderModule,
+        // which will be executed on the GPU within our compute pipeline.
+        Some(
+            self.device.create_shader_module(&wgpu::ShaderModuleDescriptor {
+                label: None,
+                source: cs_data,
+                flags: wgpu::ShaderFlags::default(),
+            })
+        )
+    }
+
+    fn create_compute_pipeline(&self, cs_module: &wgpu::ShaderModule) -> (wgpu::BindGroup, wgpu::ComputePipeline) {
+        // Setup bind groups
+        let mut group_layout_entries: Vec<wgpu::BindGroupLayoutEntry> = Vec::new();
+        let mut group_entries: Vec<wgpu::BindGroupEntry> = Vec::new();
+
+        // TODO: just assumes that all layouts are COMPUTE + rw STORAGE + ...
+        for i in 0..self.buffer_bindings.len() {
+            let b = self.buffer_bindings[i];
+
+            group_layout_entries.push(wgpu::BindGroupLayoutEntry {
+                binding: b,
+                visibility: wgpu::ShaderStage::COMPUTE,
+                ty: wgpu::BindingType::Buffer {
+                    ty: wgpu::BufferBindingType::Storage { read_only: false },
+                    has_dynamic_offset: false,
+                    min_binding_size: None,
+                },
+                count: None,
+            });
+
+            group_entries.push(wgpu::BindGroupEntry {
+                binding: b,
+                resource: self.upload_buffers.get(i).unwrap().as_entire_binding(),
+            });
+        }
+
+        let bind_group_layout = self.device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+            label: None,
+            entries: &group_layout_entries,
+        });
+
+        let bind_group = self.device.create_bind_group(&wgpu::BindGroupDescriptor {
+            label: None,
+            layout: &bind_group_layout,
+            entries: &group_entries,
+        });
+
+        // Setup pipeline
+        let compute_pipeline_layout = self.device.create_pipeline_layout(
+            &wgpu::PipelineLayoutDescriptor {
+                label: None,
+                bind_group_layouts: &[&bind_group_layout],
+                push_constant_ranges: &[],
+            }
+        );
+
+        let compute_pipeline = self.device.create_compute_pipeline(
+            &wgpu::ComputePipelineDescriptor {
+                label: None,
+                layout: Some(&compute_pipeline_layout),
+                module: &cs_module,
+                entry_point: "main",
+            }
+        );
+
+        (bind_group, compute_pipeline)
+    }
+
+    pub fn compute(&mut self, x: u32, y: u32, z: u32) {
+        // Use a CommandEncoder to batch all commands that you wish to send to the GPU to execute.
+        // The resulting CommandBuffer can then be submitted to the GPU via a Queue.
+        // Signal the end of the batch with CommandEncoder#finish().
+        let mut encoder =
+            self.device.create_command_encoder(&wgpu::CommandEncoderDescriptor { label: None });
+
+        {
+            // The compute pass will start ("dispatch") our compute shader.
+            let mut compute_pass =
+                encoder.begin_compute_pass(&wgpu::ComputePassDescriptor { label: None });
+            compute_pass.set_pipeline(self.compute_pipeline.as_ref().unwrap());
+            compute_pass.set_bind_group(0, self.bind_group.as_ref().unwrap(), &[]);
+            compute_pass.insert_debug_marker("Pasture Compute Debug");
+            compute_pass.dispatch(x, y, z);
+        }
+
+        // Copy buffers
+        {
+            for i in 0..self.upload_buffers.len() {
+                let upload = self.upload_buffers.get(i).unwrap();
+                let download = self.download_buffers.get(i).unwrap();
+                let size = self.buffer_sizes.get(i).unwrap();
+
+                encoder.copy_buffer_to_buffer(upload, 0, download, 0, *size);
+            }
+        }
+
+        // Submit to queue
+        self.queue.submit(Some(encoder.finish()));
+    }
 }
 
 // == Helper types ===============================================================================
 
+// TODO: other options to consider could be Features, Limits, ...
 pub struct DeviceOptions {
     pub device_power: DevicePower,
     pub device_backend: DeviceBackend,
@@ -127,4 +347,13 @@ pub enum DeviceBackend {
 impl Default for DeviceBackend {
     /// Default is [DeviceBackend::Primary]
     fn default() -> Self { Self::Primary }
+}
+
+// TODO: pass in actual content, usage, binding number (, size, mapped_at_creation, ...)
+// For now assume writeable storage buffer that will have an associated buffer to download from
+// Also assume mapped_at_creation to be false
+// Future: should indicate whether this buffer is for rendering or compute
+pub struct DeviceBuffer<'a> {
+    pub content: &'a [u8],
+    pub binding: u32,
 }
